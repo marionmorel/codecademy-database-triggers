8d3b56a2779811cc813c60d97b9f1d193289418f
ma-mrl <marion.mrl@live.fr>
2024-07-11 20:13:00 +0100

    update triggers

--- script.sql
+++ script.sql
@@ -4,3 +4,30 @@ ORDER BY customer_id;
 
 SELECT *
 FROM customers_log;
+
+CREATE TRIGGER customer_updated
+BEFORE UPDATE ON customers
+FOR EACH ROW
+EXECUTE PROCEDURE log_customers_change();
+
+UPDATE customers
+SET first_name = 'Colin'
+WHERE customer_id = 1;
+
+SELECT *
+FROM customers
+ORDER BY customer_id;
+
+SELECT *
+FROM customers_log;
+
+UPDATE customers
+SET home_phone = 000-555-0000
+WHERE customer_id = 1;
+
+SELECT *
+FROM customers
+ORDER BY customer_id;
+
+SELECT *
+FROM customers_log;
