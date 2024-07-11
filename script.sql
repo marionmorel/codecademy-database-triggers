c7b1f4146d21750cadf51c4cff3722a44399a561
ma-mrl <marion.mrl@live.fr>
2024-07-11 20:13:29 +0100

    insert trigger

--- script.sql
+++ script.sql
@@ -31,3 +31,24 @@ ORDER BY customer_id;
 
 SELECT *
 FROM customers_log;
+
+CREATE TRIGGER customer_insert
+AFTER INSERT ON customers
+FOR EACH STATEMENT
+EXECUTE PROCEDURE log_customers_change();
+
+INSERT INTO customers (first_name, last_name, email_address, home_phone, city, state_name, years_old)
+VALUES ('Marion', 'Morel', 'marion.morel27@gmail.com', '202-555-1072', 'Greenville', 'South Carolina', 27);
+
+INSERT INTO customers (first_name, last_name, email_address, home_phone, city, state_name, years_old)
+VALUES ('Jordan', 'Gates', 'jordangates@email.com', '202-555-0032', 'Nashville', 'Tennessee', 62);
+
+INSERT INTO customers (first_name, last_name, email_address, home_phone, city, state_name, years_old)
+VALUES ('Jane', 'Doe', 'janedoe@gmail.com', '202-555-0087', 'New York City', 'New York', 45);
+
+SELECT *
+FROM customers
+ORDER BY customer_id;
+
+SELECT *
+FROM customers_log;
